4160a7a3f77c66e260c03dc39c3025b16a2d3ebc
Biswadeepgiri <biswadeepgiri48@gmail.com>
2024-11-09 09:02:16 +0530

    made some changes in the style of the website

--- src/components/AudioRecorder.js
+++ src/components/AudioRecorder.js
@@ -100,117 +100,50 @@ const AudioRecorder = (props) => {
 
 
     return (
-        <div
-            style={{
-                border: "1px solid black",
-                backgroundColor: "black",
-                width: "700px",
-                height: "350px"
-            }}
-        >
-            <div
-                style={{
-                    border: "1px solid #bd9f61",
-                    height: "70px",
-                    backgroundColor: "#bd9f61",
-                    display: "flex"
-                }}
-            >
-                <h4
-                    style={{
-                        marginLeft: "10px",
-                        textTransform: "capitalize",
-                        fontFamily: "sans-serif",
-                        fontSize: "18px",
-                        color: "white"
-                    }}
-                >
+        <div className="border border-black bg-black w-[700px] h-[350px]">
+            <div className="border border-[#bd9f61] h-[70px] bg-[#bd9f61] flex items-center">
+                <h4 className="ml-2 capitalize font-sans text-lg text-white">
                     {status}
                 </h4>
             </div>
-            <div style={{ height: "38px" }}>
-                {" "}
-                <video src={mediaBlobUrl} controls loop />
+            <div className="h-[38px]">
+                <video src={mediaBlobUrl} controls loop className="w-full h-full" />
             </div>
 
-            <div
-                className="col-md-6 col-md-offset-3"
-                style={{
-                    backgroundColor: "black",
-                    color: "white",
-                    marginLeft: "357px"
-                }}
-            >
+            <div className="col-md-6 col-md-offset-3 bg-black text-white ml-[357px]">
                 <button
-                    style={{
-                        backgroundColor: "black",
-                        borderRadius: "8px",
-                        color: "white"
-                    }}
+                    className="bg-black rounded-lg text-white"
                     onClick={stopTimer}
                 >
                     Clear
                 </button>
-                <div style={{ marginLeft: "70px", fontSize: "54px" }}>
+                <div className="ml-16 text-6xl flex items-center">
                     <span className="minute">{minute}</span>
                     <span>:</span>
                     <span className="second">{second}</span>
                 </div>
 
-                <div style={{ marginLeft: "20px", display: "flex" }}>
-                    <label
-                        style={{
-                            fontSize: "15px",
-                            fontWeight: "Normal"
-                            // marginTop: "20px"
-                        }}
-                        htmlFor="icon-button-file"
-                    >
-                        <h3 style={{ marginLeft: "15px", fontWeight: "normal" }}>
+                <div className="ml-5 flex items-center">
+                    <label className="text-sm font-normal" htmlFor="icon-button-file">
+                        <h3 className="ml-4 font-normal">
                             Press the Start to record
                         </h3>
-
                         <div>
                             <button
-                                style={{
-                                    padding: "0.8rem 2rem",
-                                    border: "none",
-                                    marginLeft: "15px",
-                                    fontSize: "1rem",
-                                    cursor: "pointer",
-                                    borderRadius: "5px",
-                                    fontWeight: "bold",
-                                    backgroundColor: "#42b72a",
-                                    color: "white",
-                                    transition: "all 300ms ease-in-out",
-                                    transform: "translateY(0)"
-                                }}
+                                className="px-8 py-3 ml-4 text-lg cursor-pointer rounded-md font-bold bg-[#42b72a] text-white transition-transform transform hover:scale-105"
                                 onClick={() => {
                                     if (!isActive) {
                                         startRecording();
                                     } else {
                                         pauseRecording();
                                     }
-
                                     setIsActive(!isActive);
                                 }}
                             >
                                 {isActive ? "Pause" : "Start"}
                             </button>
                             <button
-                                style={{
-                                    padding: "0.8rem 2rem",
-                                    border: "none",
-                                    backgroundColor: "#df3636",
-                                    marginLeft: "15px",
-                                    fontSize: "1rem",
-                                    cursor: "pointer",
-                                    color: "white",
-                                    borderRadius: "5px",
-                                    fontWeight: "bold",
-                                    transition: "all 300ms ease-in-out",
-                                    transform: "translateY(0)"
-                                }}
+                                className="px-8 py-3 ml-4 text-lg cursor-pointer rounded-md font-bold bg-[#df3636] text-white transition-transform transform hover:scale-105"
                                 onClick={() => {
                                     stopRecording();
                                     pauseRecording();
@@ -221,9 +154,9 @@ const AudioRecorder = (props) => {
                         </div>
                     </label>
                 </div>
-                <b></b>
             </div>
         </div>
     );
+
 };
 export default AudioRecorder;
